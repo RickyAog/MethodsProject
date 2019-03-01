484a0930507e0f9f48413fffd308dabd58518689
Ricky <richard.anderson.2021@student.lhschools.org>
2019-03-01 14:17:37 -0500

    Created MethodsProject

---
+++ src/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+
+    public static String helloPerson(String personName) {
+
+        return ("HELLO" + personName);
+    }
+}
