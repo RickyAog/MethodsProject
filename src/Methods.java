16bbdcd397756cfc67d8822a52f2b43f6fdf2f87
Ricky <richard.anderson.2021@student.lhschools.org>
2019-03-11 14:16:57 -0400

    Created 4/6 methods

--- src/Methods.java
+++ src/Methods.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class Methods {
     public static void main(String[] args) {
+Scanner keyboard;
+keyboard=new Scanner (System.in);
+System.out.println(helloWorld());
+System.out.println(helloPerson());
+System.out.println(multiply());
 
-        System.out.println();
+
+System.out.println(worldHello());
     }
-
-    public static String helloPerson(String personName) {
-
-        return ("HELLO" + personName);
+    public static String helloWorld(){
+        return("Hello World");
     }
+    public static String helloPerson() {
+        String personName;
+        Scanner keyboard;
+        keyboard=new Scanner (System.in);
+        personName = keyboard.nextLine();
+        return("Hello " + personName);
+        }
+        public static int multiply(){
+        return(186*2);
+        }
+        public static String worldHello(){
+        return("WorldHello");
+        }
+
+
+
+
+
 }
