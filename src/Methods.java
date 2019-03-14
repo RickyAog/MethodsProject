95cbb92c44d5a4f88202de50bab4f9f590ba99f2
Ricky <richard.anderson.2021@student.lhschools.org>
2019-03-14 13:56:25 -0400

    Close to Being Done

--- src/Methods.java
+++ src/Methods.java
@@ -2,31 +2,55 @@ import java.util.Scanner;
 
 public class Methods {
     public static void main(String[] args) {
-Scanner keyboard;
-keyboard=new Scanner (System.in);
+
 System.out.println(helloWorld());
 System.out.println(helloPerson());
 System.out.println(multiply());
-
-
+System.out.println(divideNumbers());
+System.out.println(doubleIT());
 System.out.println(worldHello());
     }
     public static String helloWorld(){
         return("Hello World");
     }
     public static String helloPerson() {
+        System.out.println("Please give me a name");
         String personName;
         Scanner keyboard;
         keyboard=new Scanner (System.in);
         personName = keyboard.nextLine();
         return("Hello " + personName);
         }
-        public static int multiply(){
-        return(186*2);
+        public static float multiply(){
+          System.out.println("Give me 2 numbers please they can be decimals");
+        float multiplyOne,multiplyTwo;
+          Scanner keyboard;
+            keyboard=new Scanner (System.in);
+            multiplyOne= keyboard.nextFloat();
+            multiplyTwo= keyboard.nextFloat();
+        return(multiplyOne * multiplyTwo);
         }
         public static String worldHello(){
-        return("WorldHello");
+        System.out.println("Give me a greetings and someone/something that is YOUR world");
+        String world,hello;
+        Scanner keyboard;
+            keyboard=new Scanner (System.in);
+            hello= keyboard.nextLine();
+            world=keyboard.nextLine();
+        return(world + hello);
         }
+        public static float divideNumbers (){
+        System.out.println("Give me 2 numbers please, they can be decimals");
+        float numberUno,numberDos;
+            Scanner keyboard;
+            keyboard=new Scanner (System.in);
+            numberUno = keyboard.nextFloat();
+            numberDos = keyboard.nextFloat();
+        return(numberUno/numberDos);
+    }
+    public static double doubleIT(){
+        return(35+68);
+    }
 
 
 
